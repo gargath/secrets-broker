0256715d0502099b6e5cf62f6838757e1e6eb412
Phil Taprogge <signup@lightweaver.info>
2021-04-25 14:18:07 +0100

    Initial chicken

---
+++ api/v1alpha1/vaultsecret_types.go
@@ -0,0 +1,85 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type SyncState string
+
+const (
+	// The in-cluster Secret is in sync with the source
+	InSyncState SyncState = "InSync"
+
+	// There was an error reconciling the Secret
+	ErrorState SyncState = "Error"
+
+	// The VaultSecret is pending reconciliation
+	PendingState SyncState = "Pending"
+
+	// The in-cluster Secret is stale, i.e. the source has changed but the Secret not been updated
+	StaleState SyncState = "Stale"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// VaultSecretSpec defines the desired state of VaultSecret
+type VaultSecretSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// VaultURL is the URL of the vault server that holds this secret. The controller must
+	// hold configuration on how to authenticate to this vault in order to retrieve the secret
+	VaultURL string `json:"vaultURL"`
+
+	// SecretPath is the path within the specified Vault where the secrest value is held
+	SecretPath string `json:"secretPath"`
+}
+
+// VaultSecretStatus defines the observed state of VaultSecret
+type VaultSecretStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	State SyncState `json:"state"`
+}
+
+// +kubebuilder:object:root=true
+
+// VaultSecret is the Schema for the vaultsecrets API
+type VaultSecret struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   VaultSecretSpec   `json:"spec,omitempty"`
+	Status VaultSecretStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// VaultSecretList contains a list of VaultSecret
+type VaultSecretList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []VaultSecret `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&VaultSecret{}, &VaultSecretList{})
+}
